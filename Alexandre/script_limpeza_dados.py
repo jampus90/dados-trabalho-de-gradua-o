286511cae85022bf17a8d9b8daee3c831680f2be
jampus90 <alexandre.yama2@gmail.com>
2025-10-24 23:46:56 -0300

    feat: script de limpeza de dados funcionando

--- Alexandre/script_limpeza_dados.py
+++ Alexandre/script_limpeza_dados.py
@@ -10,11 +10,16 @@ csv_files_list = list(original_csv_paths.glob('*.csv'))
 
 for csv in csv_files_list:
     # Le dados
-    df = pd.read_fwf(csv)
+    df = pd.read_csv(csv, sep='\t')
     
+    # Tira as linhas com a Ellie
     df_cleaned = df[df['speaker'] != 'Ellie']
-    df_cleaned = df_cleaned.drop(['start_time', 'stop_time'], axis=1)
+
+    # Tira as colunas que não queremos usar
+    df_cleaned = df_cleaned.drop(['start_time', 'stop_time', 'speaker'], axis=1)
 
     new_csv_path = cleaned_csv_path / csv.name
 
     df_cleaned.to_csv(new_csv_path, index=False)
+
+print("Done!")
